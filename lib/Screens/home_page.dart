48a5d4fc44f14aa93701668a59ab9a7f050e1a58
SrBrago <161646897+PabloRamas@users.noreply.github.com>
2024-04-30 16:46:51 -0600

    Initial commit

---
+++ lib/Screens/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:coloredcontainer/coloredcontainer.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+    body: Center(
+      child: ColoredContainer(
+          child: Text('some text')
+      ),),
+    );
+  }
+}
